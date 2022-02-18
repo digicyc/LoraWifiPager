5fcd3e6872b87fc096c50fd5ae52b66b27816f25
AaronA <digicyc@gmail.com>
2022-02-18 01:55:51 -0700

    Some changes to the button handling.

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,34 @@
 #include "graphics.h"
 #include "secrets.h"
 
-#define BUT 17 // One button to rule them all.
+#define DEBOUNCE_TIME 50
+
+#define BUT1 17 // Right
+#define BUT2 12 // Middle
+#define BUT3 13 // left
+
 
 void display_ip();
 
 
 U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ ESPSCL, /* data=*/ ESPSDA, /* reset=*/ RST);
+
+int lastSteadyStateBut1 = LOW;
+int lastFlickerableStateBut1 = LOW;
+int but1State;
+
+int lastSteadyStateBut2 = LOW;
+int lastFlickerableStateBut2 = LOW;
+int but2State;
+
+int lastSteadyStateBut3 = LOW;
+int lastFlickerableStateBut3 = LOW;
+int but3State;
+
+unsigned long lastDebounceTimeBut1 = 0;
+unsigned long lastDebounceTimeBut2 = 0;
+unsigned long lastDebounceTimeBut3 = 0;
+
 
 
 void setupWifi() {
@@ -64,7 +86,9 @@ void setup() {
   Serial.begin(115200);
   EEPROM.begin(512); // 512 bytes out of 4096Bytes (4KB)
 
-  pinMode(BUT, INPUT_PULLUP);
+  pinMode(BUT1, INPUT_PULLUP);
+  pinMode(BUT2, INPUT_PULLUP);
+  pinMode(BUT3, INPUT_PULLUP);
 
   u8x8.begin();
   u8x8.setFont(u8x8_font_chroma48medium8_r);
@@ -74,18 +98,63 @@ void setup() {
   setupWifi();
 }
 
-int lastState = HIGH;
-int currentState;
 
 void loop() {
-  currentState = digitalRead(BUT);
+  but1State = digitalRead(BUT1);
+  but2State = digitalRead(BUT2);
+  but3State = digitalRead(BUT3);
 
-  if(lastState == LOW && currentState == HIGH) {
-    Serial.println("One Button To Rule them ALL!!!!!\n");
-    u8x8.clearDisplay();
-    u8x8.drawString(0, 1, "Button Pushed!");
+  // Button1
+  if( but1State != lastFlickerableStateBut1) {
+    lastDebounceTimeBut1 = millis();
+    lastFlickerableStateBut1 = but1State;
   }
-  lastState = currentState;
+
+  if((millis() - lastDebounceTimeBut1) > DEBOUNCE_TIME) {
+    if(lastSteadyStateBut1 == HIGH && but1State == LOW) {
+      Serial.println("Button1 Pressed!");
+      u8x8.clearDisplay();
+      u8x8.drawString(0, 1, "Button 1 Pushed!");
+    } else if(lastSteadyStateBut1 == LOW && but1State == HIGH) {
+      Serial.println("Button1 is released");
+    }
+    lastSteadyStateBut1 = but1State;
+  }
+
+  // Button2
+  if( but2State != lastFlickerableStateBut2) {
+    lastDebounceTimeBut2 = millis();
+    lastFlickerableStateBut2 = but2State;
+  }
+
+  if((millis() - lastDebounceTimeBut2) > DEBOUNCE_TIME) {
+    if(lastSteadyStateBut2 == HIGH && but2State == LOW) {
+      Serial.println("Button2 Pressed!");
+      u8x8.clearDisplay();
+      u8x8.drawString(0, 1, "Button 2 Pushed!");
+    } else if(lastSteadyStateBut2 == LOW && but2State == HIGH) {
+      Serial.println("Button2 is released");
+    }
+    lastSteadyStateBut2 = but2State;
+  }
+
+  // Button3
+  if( but3State != lastFlickerableStateBut1) {
+    lastDebounceTimeBut3 = millis();
+    lastFlickerableStateBut3 = but3State;
+  }
+
+  if((millis() - lastDebounceTimeBut3) > DEBOUNCE_TIME) {
+    if(lastSteadyStateBut3 == HIGH && but3State == LOW) {
+      Serial.println("Button3 Pressed!");
+      u8x8.clearDisplay();
+      u8x8.drawString(0, 1, "Button 3 Pushed!");
+    } else if(lastSteadyStateBut3 == LOW && but3State == HIGH) {
+      Serial.println("Button3 is released");
+    }
+    lastSteadyStateBut3 = but3State;
+  }
+
 
   if(WiFi.status() != WL_CONNECTED) {
     Serial.println("Wifi Disconnected!");
